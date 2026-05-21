fd34f71d82430d73c7de34797db44fe42ebe297a
kooone100 <iamphyque@gmail.com>
2026-05-21 04:44:02 +0000

    3rd Update

--- API/settings.py
+++ API/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'django_filters',
     'cloudinary',
     'cloudinary_storage',
 ]
