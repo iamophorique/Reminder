729e784d5c81d339bb558d10e57f70ce122999cb
kooone100 <iamphyque@gmail.com>
2026-05-26 17:07:49 +0000

    Psycopg2 check

--- API/urls.py
+++ API/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mindApp.views import LogoutView
+#   from mindApp.views import LogoutView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -33,5 +33,5 @@ urlpatterns = [
     # JWT endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/logout/', LogoutView.as_view(), name='logout')
+    #   path('api/logout/', LogoutView.as_view(), name='logout')
 ]
